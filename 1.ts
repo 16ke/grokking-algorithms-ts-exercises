1bc4a3601d9cca4371e7621e513bdf3b32e60b1e
16ke <166037413+16ke@users.noreply.github.com>
2024-11-02 15:58:53 +0000

    Delete 1.ts

--- 1.ts
+++
@@ -1,9 +0,0 @@
-function sumList(list: number[]): number {
-    if (list.length === 0) {
-        return 0;
-    }
-    return list[0] + sumList(list.slice(1));
-}
-
-const numbers = [2, 4, 6];
-console.log(sumList(numbers));
