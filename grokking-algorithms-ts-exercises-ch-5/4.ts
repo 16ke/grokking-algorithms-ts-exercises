71661838ac849ea5357ade28a3f8e990d3c4388e
16ke <166037413+16ke@users.noreply.github.com>
2025-04-03 15:29:49 +0100

    Create 4.ts

---
+++ grokking-algorithms-ts-exercises-ch-5/4.ts
@@ -0,0 +1,7 @@
+function hashFunction4(x: string): number {
+    return x.length; 
+}
+
+console.log(hashFunction4("dog"));
+console.log(hashFunction4("cat")); 
+console.log(hashFunction4("parrot")); 
